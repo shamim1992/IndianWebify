6a5df3bdbacea27c35ee252005338b0259ca1599
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-04-30 17:22:36 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Blog from './pages/Blog'
 import Contact from './pages/Contact'
 import Services from './pages/Services'
+import NotFound from './pages/NotFound'
 function App() {
 
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route path='/services' element={<Services />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
     </BrowserRouter>
        
